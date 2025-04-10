bd20e1fbc58505f684ff8d218864a4ad1ee86397
aldenor-neto <aldenor05062010@gmail.com>
2025-04-10 17:22:31 -0300

    created authorization

--- src/main/java/com/aldenor_neto/devout_catholic/config/TokenService.java
+++ src/main/java/com/aldenor_neto/devout_catholic/config/TokenService.java
@@ -4,6 +4,7 @@ import com.aldenor_neto.devout_catholic.model.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -16,7 +17,7 @@ import org.springframework.stereotype.Service;
 public class TokenService {
 
     @Value("${api.security.token.secret}")
-private String secret;
+    private String secret;
 
     public String gerarToken(User usuario) {
         try {
@@ -34,4 +35,18 @@ private String secret;
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(12).toInstant(ZoneOffset.of("-03:00"));
     }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
 }
